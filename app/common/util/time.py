8e2b731fb3fd6947911731d664c820299b030d17
kcw2297 <kcw2371@gmail.com>
2024-10-28 23:20:03 +0900

    GROW-81 fix: 자산 등록 시, 구매일자 주말 비허용

--- app/common/util/time.py
+++ app/common/util/time.py
@@ -3,6 +3,10 @@ import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+def check_weekend():
+    today = datetime.today().weekday()
+    return today >= 5 
+    
 
 def get_now_date():
     seoul_tz = ZoneInfo("Asia/Seoul")
