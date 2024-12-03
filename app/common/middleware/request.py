052b7cd6a0377fe456b0b2c74d0cf0182a028437
kcw2297 <kcw2371@gmail.com>
2024-12-04 01:09:28 +0900

    GROW-243 fix: 테스트용 요청 처리 시간 로그 삭제

--- app/common/middleware/request.py
+++ app/common/middleware/request.py
@@ -15,15 +15,6 @@ load_dotenv()
 ENVIRONMENT = getenv("ENVIRONMENT", None)
 
 
-logger = logging.getLogger("request_middleware")
-logger.setLevel(logging.INFO)
-
-if ENVIRONMENT == EnvironmentType.PROD:
-    file_handler = logging.FileHandler("/home/backend/request.log", delay=False)
-    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-    logger.addHandler(file_handler)
-
-
 class TimeoutMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         try:
@@ -31,14 +22,3 @@ class TimeoutMiddleware(BaseHTTPMiddleware):
         except asyncio.TimeoutError:
             raise HTTPException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, detail="요청 시간이 초과 되었습니다.")
 
-
-class RequestTimerMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        start_time = time.time()
-        response: Response = await call_next(request)
-        process_time = time.time() - start_time
-
-        logger.info(f"요청 URL: {request.url.path} | " f"상태 코드: {response.status_code} | " f"처리 시간: {process_time:.4f} 초")
-        response.headers["X-Process-Time"] = str(process_time)
-
-        return response
